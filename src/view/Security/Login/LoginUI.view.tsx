803fabf5771f0cdf9b48de3301e1fd904525d8cf
tomasfqit <tomas.quichimbo@tomebamba.com.ec>
2026-03-02 11:03:15 -0500

    Update package dependencies to use the latest version of tomascomponents, modify README for installation instructions, and add GitHub Actions workflow for deploying to GitHub Pages.

--- src/view/Security/Login/LoginUI.view.tsx
+++ src/view/Security/Login/LoginUI.view.tsx
@@ -1,6 +1,6 @@
 import { Button, FormInput } from "tomascomponents";
 import type { ILoginUI } from "./LoginUI.hook";
-import { IconCarSuv  } from "../../../assets";
+import { IconCarSuv } from "../../../assets";
 
 export const LoginUIView = ({
   control,
@@ -8,18 +8,22 @@ export const LoginUIView = ({
   isSignInPending,
 }: ILoginUI) => {
   return (
-    <div className="flex-1w-full min-h-0 h-full">
-      
-      <div className="flex flex-col items-center justify-center gap-2 bg-gray-900 border border-gray-700 min-w-96 h-96 rounded-4xl p-4">
-        <div className="shadow-md shadow-white rounded-4xl rotate-12 p-2">
-          <div className="flex items-center justify-center">
-            <IconCarSuv width={100} height={100} color="white" />
+    <div className=" flex flex-col items-center justify-center w-full min-h-screen bg-black pr-2 pl-2">
+      <div className="w-full md:w-[500px] gap-2 border-gray-300 border-2 rounded-4xl p-4">
+        <div className="flex flex-col items-center justify-center w-full gap-2 text-white">
+            <div className="flex flex-col items-center justify-center gap-0 rotate-12 mb-2">
+              <IconCarSuv width={100} height={100} color="white" />
+              <div className="bg-white w-full h-0.5 -mt-6"/>
+            </div>
+          <div className="w-full md:w-[400px]">
+            <FormInput label="Email" control={control} name="email" placeholder="Email" />
           </div>
-        </div>
-        <FormInput label="Email" control={control} name="email" placeholder="Email" />
-        <FormInput label="Password" control={control} name="password" placeholder="Password" />
-        <div className="flex justify-center pt-4">
-          <Button onClick={handleFormSubmit} title="Login" loading={isSignInPending} />
+          <div className="w-full md:w-[400px]">
+            <FormInput label="Password" control={control} name="password" placeholder="Password" />
+          </div>
+          <div className="flex justify-center pt-4 w-full">
+            <Button onClick={handleFormSubmit} title="Login" loading={isSignInPending} />
+          </div>
         </div>
       </div>
     </div>
