83436ce2da229e0c34f0e76cef9d5cb0439f0e6a
tomasfqit <tomas.quichimbo@tomebamba.com.ec>
2026-03-02 12:17:03 -0500

    Replace BrowserRouter with HashRouter for improved routing compatibility in main application file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import { App } from './App.tsx'
 import { QueryProvider } from './provider/QueryProvider.tsx'
@@ -8,9 +8,9 @@ import { QueryProvider } from './provider/QueryProvider.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryProvider>
   </StrictMode>,
 )
