fc1a5817283ae99e390479b86903c2c3b1792593
tomasfqit <tomas.quichimbo@tomebamba.com.ec>
2026-03-20 11:55:34 -0500

    Update tomascomponents dependency to version 0.0.4, add constants for vehicle brands, models, and colors, and enhance VehicleCreateUI components to utilize these constants for improved form handling and selection options.

--- src/view/Main/Home/components/Vehicles/components/create/VehicleCreateUI.hook.tsx
+++ src/view/Main/Home/components/Vehicles/components/create/VehicleCreateUI.hook.tsx
@@ -2,18 +2,29 @@ import type { Control } from "react-hook-form";
 import type { VehicleInsert } from "../../../../../../../services/vehiculo.service";
 import { useVehicleCreateForm } from "./form/useVehicleCreateForm";
 import { useModal } from "tomascomponents";
+import { useMemo } from "react";
+import { BRANDS } from "../../../../../../../constants/Brands";
+import { MODELS } from "../../../../../../../constants/Models";
+import type { DefaultOptionType } from "antd/es/select";
+import { COLORS } from "../../../../../../../constants/Colors";
+
 
 export interface IVehicleCreateUI {
   control: Control<VehicleInsert>;
   closeModal: () => void;
   handleFormSubmit: () => void;
   loading?: boolean;
+  brandsOptions: DefaultOptionType[];
+  modelsOptions: DefaultOptionType[];
+  colorsOptions: DefaultOptionType[];
 }
 
 export const useVehicleCreateUI = (): IVehicleCreateUI => {
   const { methods } = useVehicleCreateForm();
   const { closeModal } = useModal();
-  const { control, handleSubmit } = methods;
+  const { control, handleSubmit, watch } = methods;
+
+  const brand = watch("marca");
 
   const onSubmit = async (data: VehicleInsert) => {
     console.log(data);
@@ -23,11 +34,39 @@ export const useVehicleCreateUI = (): IVehicleCreateUI => {
     handleSubmit(onSubmit)();
   };
 
+  const brandsOptions = useMemo(() => {
+    return BRANDS.map((brand) => ({
+      label: brand.name,
+      value: brand.id,
+    }));
+  }, []);
+
+  // filtrado de los modelos
+  const modelsOptions = useMemo(() => {
+    const selectedBrand = BRANDS.find((b) => `${b.id}` === `${brand}`);
+    if (!selectedBrand) return [];
+    const modelsByBrand = MODELS[selectedBrand.name] ?? MODELS.Otra ?? [];
+    return modelsByBrand.map((model) => ({
+      label: model.name,
+      value: model.id,
+    }));
+  }, [brand]);
+
+  const colorsOptions = useMemo(() => {
+    return COLORS.map((color) => ({
+      label: color.name,
+      value: color.id,
+    }));
+  }, []);
+
 
   return {
     control,
     closeModal,
     handleFormSubmit,
     loading: false,
+    brandsOptions,
+    modelsOptions,
+    colorsOptions,
   };
 };
--- src/view/Main/Home/components/Vehicles/components/create/VehicleCreateUI.view.tsx
+++ src/view/Main/Home/components/Vehicles/components/create/VehicleCreateUI.view.tsx
@@ -1,22 +1,22 @@
-import { FormInput } from "tomascomponents";
+import { FormInput, FormNumberInput, FormSelect } from "tomascomponents";
 import type { IVehicleCreateUI } from "./VehicleCreateUI.hook";
 import { Form } from "react-hook-form";
 import { CustomFooterModal } from "../../../../../../../components/Render/CustomFooterModal";
 
-export const VehicleCreateUIView = ({ control, handleFormSubmit, closeModal, loading }: IVehicleCreateUI) => {
+export const VehicleCreateUIView = ({ control, handleFormSubmit, closeModal, loading, brandsOptions, modelsOptions, colorsOptions }: IVehicleCreateUI) => {
     return (
-        <div className="flex h-full w-full max-w-full min-h-0 flex-col gap-2 overflow-x-hidden md:w-[40vw]">
-            <div className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden pr-1">
-                <Form onSubmit={handleFormSubmit} control={control}>
-                    <FormInput label="Placa" name="placa" placeholder="Placa" control={control} />
-                    <FormInput label="Marca" name="marca" placeholder="Marca" control={control} />
-                    <FormInput label="Modelo" name="modelo" placeholder="Modelo" control={control} />
-                    <FormInput label="Año" name="anio" placeholder="Año" control={control} />
-                    <FormInput label="Color" name="color" placeholder="Color" control={control} />
-                    <FormInput label="Propietario ID" name="propietario_id" placeholder="Propietario ID" control={control} />
-                </Form>
-            </div>
-            <CustomFooterModal onCancel={closeModal} onConfirm={handleFormSubmit} loading={loading} />
+      <div className="flex h-full w-full max-w-full min-h-0 flex-col gap-2 overflow-x-hidden md:w-[40vw]">
+        <div className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden pr-1">
+          <Form onSubmit={handleFormSubmit} control={control}>
+            <FormInput label="Placa" name="placa" placeholder="Placa" control={control} />
+            <FormSelect label="Marca" name="marca" placeholder="Marca" control={control} options={brandsOptions} />
+            <FormSelect label="Modelo" name="modelo" placeholder="Modelo" control={control} options={modelsOptions} />
+            <FormNumberInput label="Año" name="anio" placeholder="Año" control={control} />
+            <FormSelect label="Color" name="color" placeholder="Color" control={control} options={colorsOptions} />
+            <FormInput label="Propietario ID" name="propietario_id" placeholder="Propietario ID" control={control} />
+          </Form>
         </div>
-    )
+        <CustomFooterModal onCancel={closeModal} onConfirm={handleFormSubmit} loading={loading} />
+      </div>
+    );
 }
